ca7c0036f8aa4b5824a145e2a9cc99cb21bf8c1e
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-02-20 12:31:32 +0100

    First route & pages

--- app/page.js
+++ app/page.js
@@ -1,9 +1,26 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
-      <h1 style={{ color: 'white', textAlign: 'center' }}>
+      <h1 style={{ color: "white", textAlign: "center" }}>
         Time to get started!
       </h1>
+      <p>
+        <Link href="/meals">
+          <button>Meals</button>
+        </Link>
+      </p>
+      <p>
+        <Link href="/meals/share">
+          <button>Share a meal</button>
+        </Link>
+      </p>
+      <p>
+        <Link href="/community">
+          <button>Community</button>
+        </Link>
+      </p>
     </main>
   );
 }

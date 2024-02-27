ebea8dff4533d1622ba60de5b3f3ccda3b469485
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-02-27 12:53:46 +0100

    Output Meals Data & Images w/Unknown Dimensions( fill)

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,23 @@
+import Link from "next/link";
+import MealsGrid from "@/components/meals/meals-grid";
+import classes from "./page.module.css";
+
 export default function MealsPage() {
   return (
-    <main>
-      <h1>Meals Page</h1>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>Choose your favorite recipes and cook it yourself.</p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your favorite recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
   );
 }

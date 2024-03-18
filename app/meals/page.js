c546931b6880b81bb8884b549970fbfdd0003988
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-03-18 13:04:56 +0100

    (feat) UI/UX: Static & Dynamic Metadata

--- app/meals/page.js
+++ app/meals/page.js
@@ -3,8 +3,13 @@ import Link from "next/link";
 
 import MealsGrid from "@/components/meals/meals-grid";
 import { getMeals } from "@/lib/meals";
-
 import classes from "./page.module.css";
+
+// ______________________________ METADATA ___________________________
+export const metadata = {
+  title: "All Meals",
+  description: "Browse the delicious meals share by our vibrant community.",
+};
 
 // ______________________________________________________________________
 async function Meals() {
